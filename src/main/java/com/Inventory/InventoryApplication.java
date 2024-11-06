cac3090b670839789f96663de20ede16dd4796dc
prasadkonjarla <konjarlaprasad432@gmail.com>
2024-11-06 17:53:02 +0530

    changes in application main

--- src/main/java/com/Inventory/InventoryApplication.java
+++ src/main/java/com/Inventory/InventoryApplication.java
@@ -2,12 +2,20 @@ package com.Inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
 public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
 
+	@GetMapping
+	public String String () {
+		return "welcome to InventoryApplication ";
+	}
+	
 }
